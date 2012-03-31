c22990130c77c93588a4585b7785afd389dba13a
Georges Dubus <georges.dubus@compiletoi.net>
2012-04-01 00:52:15 +0200

    Awesome new css.

--- ftp/views.py
+++ ftp/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from ftp.models import FtpServer, File
 
+def all_servers():
+    return list(FtpServer.objects.order_by('-online', '-size'))
 
 def index(request):
-    servers = FtpServer.objects.order_by('-online', '-size')
     return render_to_response(
         'ftp/index.html',
-        {'servers': list(servers), 'active_server': None}
+        {'servers': all_servers(), 'active_server': None}
     )
 
 
 def server(request, address):
-    servers = FtpServer.objects.all()
     server = get_object_or_404(FtpServer, address=address)
     files = server.files.order_by('name')
     return render_to_response(
         'ftp/server.html',
-        {'servers': list(servers), 'active_server': server, 'files': list(files)}
+        {'servers': all_servers(), 'active_server': server, 'files': list(files)}
     )
 
 
@@ -27,7 +27,7 @@ def search(request):
         files = File.objects.filter(name__contains=query)
         return render_to_response(
             'ftp/search.html',
-            {'files': list(files), 'query': query}
+            {'servers': all_servers(), 'files': list(files), 'query': query}
         )
     except KeyError:
         return redirect('ftp.views.index')
