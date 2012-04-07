02c1819eff13a4e1d9d9e1547e7e8309184ce349
remram44 <remirampin@gmail.com>
2012-04-07 18:04:50 +0200

    More iptools stuff: IP now a class, iteration logic.

--- indexer/iptools.py
+++ indexer/iptools.py
@@ -6,23 +6,59 @@ class InvalidAddress(ValueError):
     pass
 
 
-def IP(i):
-    if type(i) == str:
-        c = i.split('.')
-        if len(c) != 4:
-            raise InvalidAddress("Not in IPv4 format")
-        try:
-            c = [int(v) for v in c]
-        except ValueError:
-            raise InvalidAddress("Invalid number")
-        for v in c:
-            if v < 0 or v >= 256:
-                raise InvalidAddress("Byte not in [0-255]")
-        return ((c[0]*256 + c[1])*256 + c[2])*256 + c[3]
-    elif type(i) == int or type(i) == long:
-        return i
-    else:
-        raise TypeError("Expected str or int, got %s" % type(i))
+@total_ordering
+class IP:
+    def __init__(self, i):
+        if isinstance(i, IP):
+            self.num = i.num
+        elif type(i) == str:
+            c = i.split('.')
+            if len(c) != 4:
+                raise InvalidAddress("Not in IPv4 format")
+            try:
+                c = [int(v) for v in c]
+            except ValueError:
+                raise InvalidAddress("Invalid number")
+            for v in c:
+                if v < 0 or v >= 256:
+                    raise InvalidAddress("Byte not in [0-255]")
+            self.num = ((c[0]*256 + c[1])*256 + c[2])*256 + c[3]
+        elif type(i) == int or type(i) == long:
+            self.num = i
+        else:
+            raise TypeError("Expected str or int, got %s" % type(i))
+
+    def __lt__(self, other):
+        return self.num < other.num
+
+    def __eq__(self, other):
+        return self.num == other.num
+
+    def __str__(self):
+        return "%d.%d.%d.%d" % (
+            (self.num >> 24) & 0xFF,
+            (self.num >> 16) & 0xFF,
+            (self.num >>  8) & 0xFF,
+            (self.num      ) & 0xFF)
+
+    def __int__(self):
+        return self.num
+
+
+class IPRangeIterator:
+    def __init__(self, range):
+        self.range = range
+        self.pos = range.first.num
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.pos > self.range.last.num:
+            raise StopIteration
+        else:
+            self.pos += 1
+            return IP(self.pos-1)
 
 
 @total_ordering
@@ -33,8 +69,13 @@ class IPRange:
             first, last = first
         elif not last:
             last = first
-        self.first = IP(first)
-        self.last = IP(last)
+
+        self.first = first
+        if not isinstance(self.first, IP):
+            self.first = IP(self.first)
+        self.last = last
+        if not isinstance(self.last, IP):
+            self.last = IP(self.last)
 
     def __lt__(self, other):
         return self.first < other.first
@@ -43,9 +84,12 @@ class IPRange:
         return self.first == other.first
 
     def contains(self, ip):
-        if type(ip) != int and type(ip) != long:
+        if not isinstance(ip, IP):
             ip = IP(ip)
         return self.first <= ip and ip <= self.last
+
+    def __iter__(self):
+        return IPRangeIterator(self)
 
 
 class IPSet:
@@ -53,7 +97,7 @@ class IPSet:
         self.ranges = []
 
     def add(self, range):
-        if type(range) != IPRange:
+        if not isinstance(range, IPRange):
             range = IPRange(range)
         self.ranges.append(range)
         # TODO : Compact this stuff!
@@ -65,3 +109,7 @@ class IPSet:
                 return True
 
         return False
+
+    def intersection(self, other):
+        # TODO
+        pass
--- indexer/tests.py
+++ indexer/tests.py
@@ -1,17 +1,37 @@
 import unittest
-from iptools import IP, IPSet, InvalidAddress
+from iptools import IP, IPRange, IPSet, InvalidAddress
 
 
 class TestIPTools(unittest.TestCase):
     def test_ip(self):
-        self.assertEqual(IP('0.0.0.0'), 0)
-        self.assertEqual(IP('1.2.3.4'), 16909060)
+        self.assertEqual(IP('0.0.0.0').num, 0)
+        self.assertEqual(IP('1.2.3.4').num, 16909060)
         self.assertRaises(InvalidAddress, lambda: IP('1.2.3'))
         self.assertRaises(InvalidAddress, lambda: IP('1.256.3.4'))
         self.assertRaises(InvalidAddress, lambda: IP('1.-2.3.4'))
         self.assertRaises(InvalidAddress, lambda: IP('1.2.3,0.4'))
 
-    # TODO : IPRange tests
+    def test_range_init(self):
+        range = IPRange('192.168.0.1')
+        self.assertEqual(range, IPRange('192.168.0.1', '192.168.0.1'))
+        self.assertEqual(range, IPRange(['192.168.0.1', '192.168.0.1']))
+
+    def test_range(self):
+        range = IPRange('10.8.0.1', '10.8.2.255')
+        self.assertFalse(range.contains('10.7.255.4'))
+        self.assertFalse(range.contains('10.8.3.0'))
+        self.assertTrue(range.contains('10.8.2.1'))
+        self.assertTrue(range.contains('10.8.1.1'))
+
+    def test_range_iter(self):
+        range = IPRange('10.8.255.254', '10.9.0.2')
+        iter = range.__iter__()
+        self.assertEqual(iter.next(), IP('10.8.255.254'))
+        self.assertEqual(iter.next(), IP('10.8.255.255'))
+        self.assertEqual(iter.next(), IP('10.9.0.0'))
+        self.assertEqual(iter.next(), IP('10.9.0.1'))
+        self.assertEqual(iter.next(), IP('10.9.0.2'))
+        self.assertRaises(StopIteration, iter.next)
 
     def test_ipset(self):
         set = IPSet()
