b3de53f1d55e78082c3042f5c15ad98055942e55
Georges Dubus <georges.dubus@compiletoi.net>
2012-04-08 17:00:48 +0200

    Fixed the home link hardcoded to "/".

--- yoppi/urls.py
+++ yoppi/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('yoppi',
-    url(r"^$", "ftp.views.index"),
+    url(r"^$", "ftp.views.index", name="index"),
     url(r"^server/(?P<address>[a-z0-9_.-]+)(?P<path>(/.*)?)$", "ftp.views.server"),
     url(r"^search/$", "ftp.views.search"),
     url(r"^download/(?P<address>[a-z0-9_.-]+)(?P<path>(/.*)?)$", "ftp.views.download"),
