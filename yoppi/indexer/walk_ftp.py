c0717b40ef84b447166283beac0433af18aaee7d
Georges Dubus <georges.dubus@compiletoi.net>
2012-08-03 19:31:24 +0200

    Made the folders display the size of their content (fixes #19).

--- yoppi/indexer/walk_ftp.py
+++ yoppi/indexer/walk_ftp.py
@@ -3,7 +3,7 @@ import re
 from yoppi.ftp.models import File
 from django.utils.translation import ugettext
 
-MAX_DEPTH = 1000
+MAX_DEPTH = 500
 MAX_FILES = 1000000
 
 class SuspiciousFtp(Exception):
@@ -27,7 +27,8 @@ class RemoteFile:
             raise IOError("invalid LIST format\n")
         self.is_directory = m.group(1)[0] == "d"
         self.is_link = m.group(1)[0] == 'l'
-        self.size = int(m.group(4))
+        self.raw_size = int(m.group(4))
+        self.size = self.raw_size
         self.raw_name = m.group(6)
         self.name = decode(self.raw_name)
 
@@ -45,7 +46,8 @@ class RemoteFile:
     def toFile(self, server, path):
         return File(
                 server=server, path=path,
-                name=self.name, is_directory=self.is_directory, size=self.size)
+                name=self.name, is_directory=self.is_directory,
+                size=self.size)
 
     def __str__(self):
         return self.name
@@ -71,37 +73,37 @@ class FallbackDecoder(object):
             return self.decode(str)
 
 
-def yield_files(server, ftp):
+def _yield_files(server, connection, decode, path, depth):
+    if depth > MAX_DEPTH:
+        raise SuspiciousFtp(ugettext(
+            "%(server)s's directory depth is more than %(max_depth)d."
+            " It doesn't seem legit.") %
+            dict(server=server.display_name(), max_depth=MAX_DEPTH))
+
+    files = []
+    connection.dir(path, lambda line: files.append(RemoteFile(line, decode)))
+
+    # For ftp, root is '/', but for us, it's ''
+    if path == '/':
+        path = ''
+
+    for f in files:
+        if f.is_link:
+            continue
+        if f.is_directory:
+            for child in _yield_files(server, connection, decode,
+                                      '%s/%s' % (path, f.raw_name),
+                                      depth + 1):
+                f.size += child[1].size
+                yield child
+        yield decode(path), f
+
+
+def yield_files(server, connection):
     """Iterates over the ftp and yield all the files as tuples
     (path, RemoteFile)"""
-    stack = [('/', 0)]
-    files = []
     decode = FallbackDecoder().decode
-
-    def callback(line):
-        files.append(RemoteFile(line, decode))
-
-    while stack:
-        path, depth = stack.pop()
-        if depth > MAX_DEPTH:
-            raise SuspiciousFtp(ugettext(
-                    "%(server)s's directory depth is more than %(max_depth)d."
-                    " It doesn't seem legit.") %
-                    dict(server=server.display_name(), max_depth=MAX_DEPTH))
-        ftp.dir(path, callback)
-
-        # For ftp, root is '/', but for us, it's ''
-        if path == '/':
-            path = ''
-
-        for f in files:
-            if f.is_link:
-                continue
-            if f.is_directory:
-                stack.append(('%s/%s' % (path, f.raw_name), depth + 1))
-            yield decode(path), f
-
-        files = []
+    return _yield_files(server, connection, decode, '/', 0)
 
 
 def walk_ftp(server, connection, db_files):
@@ -118,7 +120,7 @@ def walk_ftp(server, connection, db_files):
                     "%(server)s has more than %(max_files)d files."
                     " It doesn't seem legit.") %
                     dict(server=server.display_name(), max_files=MAX_FILES))
-        total_size += file.size
+        total_size += file.raw_size
 
         try:
             ftp_file = db_files.pop(u'%s/%s' % (path, file.name))
